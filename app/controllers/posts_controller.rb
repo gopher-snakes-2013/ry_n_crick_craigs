c3f42af39cab706fa390e8c4eedcb65d74c361c6
Dev Bootcamp <Apprentice@dbc34.local>
2013-10-28 18:23:14 -0700

    initial commit all the things

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,40 @@
+class PostsController < ApplicationController
+
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new params[:post]
+    if @post.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def destroy
+    post = Post.find(params[:id])
+    post.destroy
+    redirect_to root_path
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update_attributes(params[:post])
+    redirect_to root_path
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Craigslist::Application.config.secret_token = 'af34a4f7a26d0b0fd04751877ee2805658f75ce087d31cdb8e28ff8c7627f293e468e11c8d5a3a8ae17b9cabf2a2a8d4c62f34223e616f8cc38be2407b76a782'
